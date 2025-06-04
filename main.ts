37faabcdbed02633a703c4b8ff708923f8366236
Oskar-Heath-Kim <oskar.heath-kim@student.csd509j.net>
2025-06-04 16:33:37 +0000

    created the hero sprite

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-//first comment
+//Constants======================================================
+const defaultPlayerHitpoints = 3
+//Global=========================================================
+
+//Classes========================================================
+
+class Hero extends sprites.ExtendableSprite {
+    hitpoints: number
+    constructor(image: Image, kind: number, hitpoints: number) { //#12
+        super(image, kind)
+        this.hitpoints = hitpoints
+    }
+    hit(points: number): void { //#13
+        this.hitpoints -= points
+        if (this.hitpoints <= 0) {
+            this.destroy()
+        }
+    }
+}
+
+//Functions======================================================
+function createPlayer(): void{
+    let player = new Hero(assets.image``,SpriteKind.Player,defaultPlayerHitpoints)
+}
+//Event-Handlers=================================================
+
+//Main===========================================================
